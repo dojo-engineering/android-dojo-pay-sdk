27806179a11feeb019a3e5b8d6deb13aa6ce43c1
Alexander Mikhalevich <>
2022-04-06 16:19:43 +0100

    Add dojo card payment activity

--- sample/src/main/java/tech/dojo/pay/sdk/CardPaymentActivity.kt
+++ sample/src/main/java/tech/dojo/pay/sdk/CardPaymentActivity.kt
@@ -1,6 +1,8 @@
 package tech.dojo.pay.sdk
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class CardPaymentActivity : AppCompatActivity() {
@@ -8,5 +10,11 @@ class CardPaymentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_payment)
+
+        findViewById<View>(R.id.btnPay).setOnClickListener {
+            startActivity(
+                Intent(this, DojoCardPaymentActivity::class.java)
+            )
+        }
     }
 }
---
+++ sdk/src/main/java/tech/dojo/pay/sdk/DojoCardPaymentActivity.kt
@@ -0,0 +1,12 @@
+package tech.dojo.pay.sdk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DojoCardPaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dojo_card_payment)
+    }
+}
