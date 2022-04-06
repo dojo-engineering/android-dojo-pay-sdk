6f1e566b3b13fe5d194212e7f7d4efa5f1270769
Alexander Mikhalevich <>
2022-04-06 16:11:16 +0100

    Add sample app base

---
+++ sample/src/main/java/tech/dojo/pay/sdk/CardPaymentActivity.kt
@@ -0,0 +1,12 @@
+package tech.dojo.pay.sdk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardPaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_payment)
+    }
+}
