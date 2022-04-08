c3f2c2775e20689ebb0fa0b23f94b44ffd4fd130
paul-dojo <paul.mitchell@paymentsense.com>
2022-04-08 16:13:02 +0100

    Added 3ds view

--- sdk/src/main/java/tech/dojo/pay/sdk/card/DojoCardPaymentEvent.kt
+++ sdk/src/main/java/tech/dojo/pay/sdk/card/DojoCardPaymentEvent.kt
@@ -7,4 +7,6 @@ internal sealed class DojoCardPaymentEvent {
     class ReturnResult(val result: DojoCardPaymentResult) : DojoCardPaymentEvent()
 
     object Navigate3DS : DojoCardPaymentEvent()
+
+    data class Show3dsScreen(val pageContent: String) : DojoCardPaymentEvent()
 }
