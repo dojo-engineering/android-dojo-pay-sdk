e9432c043e14d24f1ed0489983622e00b0b80bc1
Paul Mitchell <paul.mitchell354@gmail.com>
2022-04-09 14:01:09 +0100

    card expiry updated to month and year. Formatting added to ensure correct format

--- sdk/src/main/java/tech/dojo/pay/sdk/card/entities/DojoCardDetails.kt
+++ sdk/src/main/java/tech/dojo/pay/sdk/card/entities/DojoCardDetails.kt
@@ -5,6 +5,7 @@ import java.io.Serializable
 data class DojoCardDetails(
     val cardNumber: String,
     val cardName: String? = null,
-    val expiryDate: String? = null,
+    val expiryMonth: String? = null,
+    val expiryYear: String? = null,
     val cv2: String? = null
 ) : Serializable
