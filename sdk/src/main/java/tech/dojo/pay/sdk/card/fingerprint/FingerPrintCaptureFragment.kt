f2acfa1d0cdd342f62b6ae47fd9f1dc9af5155c0
Alexander Mikhalevich <>
2022-04-13 18:26:29 +0100

    Add event listener

--- sdk/src/main/java/tech/dojo/pay/sdk/card/fingerprint/FingerPrintCaptureFragment.kt
+++ sdk/src/main/java/tech/dojo/pay/sdk/card/fingerprint/FingerPrintCaptureFragment.kt
@@ -2,9 +2,11 @@ package tech.dojo.pay.sdk.card.fingerprint
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -29,28 +31,36 @@ internal class FingerPrintCaptureFragment private constructor(): Fragment() {
         savedInstanceState: Bundle?
     ): View = WebView(requireContext()).apply {
         settings.javaScriptEnabled = true
+        addJavascriptInterface(FingerPrintCaptureListener(), "Android")
         val html = getHtml(deviceData.token, deviceData.formAction)
         loadData(html, "text/html", "utf-8")
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        lifecycleScope.launch {
-            delay(10000)
-            viewModel.onFingerprintCaptured()
-        }
     }
 
     private fun getHtml(token: String, formAction: String): String =
         """
         <html>
             <body onload="document.forms[0].submit()">
-            <form id="ddc-form" target=”ddc-iframe”  method="POST" action="$formAction">
-                <input id="ddc-input" name="JWT" type="hidden" value="$token" />
-            </form>
-            <iframe name=”ddc-iframe” height="1" width="1"> </iframe>
+                <form id="ddc-form" target=”ddc-iframe”  method="POST" action="$formAction">
+                    <input id="ddc-input" name="JWT" type="hidden" value="$token" />
+                </form>
+                <iframe name=”ddc-iframe” height="1" width="1"> </iframe>
             </body>
+            <script>
+                window.addEventListener('message', (event) => {
+                    Android.onFingerPrintCaptured();
+                });
+            </script>
         </html>
         """.trimIndent()
+
+    private inner class FingerPrintCaptureListener {
+
+        @JavascriptInterface
+        fun onFingerPrintCaptured() {
+            Log.v("custom", "Result captured")
+            //viewModel.onFingerprintCaptured()
+        }
+    }
 
     companion object {
 
