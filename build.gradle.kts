1af9a5c5b322a0208178351acf9ace2836425068
Vyshakh <vyshakh.shanthi@paymentsense.com>
2022-06-06 16:27:53 +0530

    [#15]Standardise Repo - Add ktlint, detekt.yml for static analysis and code style checks to be done locally. - add Dojo codestyle to repo.

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    kotlin("android") apply false
+    id("io.gitlab.arturbosch.detekt") version "1.17.1"
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+}
+
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(false)
+        version.set("10.1.0")
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        enableExperimentalRules.set(true)
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
+
+    detekt {
+        config = rootProject.files("configs/detekt/detekt.yml")
+        reports {
+            xml.enabled = true
+            html.enabled = true
+            txt.enabled = true
+            sarif.enabled = true
+        }
+    }
+}
+
+tasks {
+    register("clean", Delete::class.java) {
+        delete(rootProject.buildDir)
+    }
+}
