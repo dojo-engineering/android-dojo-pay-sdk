4dffad35d62c362ca85e275e5bc6db9e77764b83
Saamyyy <71371440+Saamyyy@users.noreply.github.com>
2022-08-04 14:12:19 +0400

    UI sdk (#30)
    * add new module for ui sdk
    
    * add repositories to  settings.gradle
    
    * add MainActivity  for the ui SDK
    
    Co-authored-by: Saamyyy <saamyyps4@gmail.com>

---
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/MainActivity.kt
@@ -0,0 +1,12 @@
+package tech.dojo.pay.uisdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+
+    }
+}
