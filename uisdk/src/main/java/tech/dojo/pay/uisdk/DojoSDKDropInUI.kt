32b00a35f9a6efcefc685783fc61419794c6e543
Saamyyy <71371440+Saamyyy@users.noreply.github.com>
2022-08-05 19:05:18 +0400

    Feature/uisdk infra work (#31)
    * add Dependencies.kt and add the needed once on the ui sdk gradle file
    
    * add navigation component infra work and add also infra work for start   sdk screen
    
    * wire sample app with the UI SDK
    
    * fix wrong Dependencies.kt
    
    * update kotlin version
    
    * rename the SDK
    
    Co-authored-by: Saamyyy <saamyyps4@gmail.com>

---
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/DojoSDKDropInUI.kt
@@ -0,0 +1,15 @@
+package tech.dojo.pay.uisdk
+
+import androidx.activity.ComponentActivity
+import tech.dojo.pay.uisdk.paymentflow.handler.DojoPaymentFlowHandler
+import tech.dojo.pay.uisdk.paymentflow.handler.DojoPaymentFlowHandlerImp
+
+object DojoSDKDropInUI {
+    var sandbox: Boolean = false
+    /**
+     * Returns handler which handle payment process with UI .
+     */
+    fun createUIPaymentHandler(
+        activity: ComponentActivity,
+    ): DojoPaymentFlowHandler = DojoPaymentFlowHandlerImp(activity)
+}
--- uisdk/src/main/java/tech/dojo/pay/uisdk/paymentflow/PaymentFlowContainerActivity.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/paymentflow/PaymentFlowContainerActivity.kt
@@ -1,12 +1,13 @@
-package tech.dojo.pay.uisdk
+package tech.dojo.pay.uisdk.paymentflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tech.dojo.pay.uisdk.R
 
-class MainActivity : AppCompatActivity() {
+class PaymentFlowContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
-
     }
+
 }
