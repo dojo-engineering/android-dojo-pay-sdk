cc2c4fd74834882b8d99b0a140b738e0825cffbe
Saamyyy <71371440+Saamyyy@users.noreply.github.com>
2022-08-12 16:23:41 +0200

    Feature/uisdk phase one mange payments and add new card (#34)
    * add ManagePaymentMethods screen
    
    * add CardDetailsCheckout screen
    
    * fix code review comments
    
    * fix typo
    
    Co-authored-by: Saamyyy <saamyyps4@gmail.com>

---
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/paymentflow/ui/carddetailscheckout/CardDetailsCheckoutScreen.kt
@@ -0,0 +1,43 @@
+package tech.dojo.pay.uisdk.paymentflow.ui.carddetailscheckout
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import tech.dojo.pay.uisdk.components.AppBarIcon
+import tech.dojo.pay.uisdk.components.DojoAppBar
+import tech.dojo.pay.uisdk.components.DojoFullGroundButton
+import tech.dojo.pay.uisdk.components.TitleGravity
+
+@Composable
+fun CardDetailsCheckoutScreen(
+    onCloseClicked:  () -> Unit,
+    onBackClicked: () -> Unit,
+    onPayButtonClicked: () -> Unit
+) {
+    DojoAppBar(
+        title = "Pay With New Card",
+        titleGravity = TitleGravity.LEFT,
+        navigationIcon = AppBarIcon.back { onBackClicked() },
+        actionIcon = AppBarIcon.close { onCloseClicked() }
+    )
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        val (doneBtn) = createRefs()
+        DojoFullGroundButton(
+            modifier = Modifier.constrainAs(doneBtn) {
+                start.linkTo(parent.start, 8.dp)
+                end.linkTo(parent.end, 8.dp)
+                bottom.linkTo(parent.bottom, 32.dp)
+                width = Dimension.fillToConstraints
+            },
+            text = "pay"
+        ) { onPayButtonClicked() }
+    }
+}
---
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/paymentflow/ui/mangepaymentmethods/ManagePaymentMethodsScreen.kt
@@ -0,0 +1,45 @@
+package tech.dojo.pay.uisdk.paymentflow.ui.mangepaymentmethods
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import tech.dojo.pay.uisdk.components.AppBarIcon
+import tech.dojo.pay.uisdk.components.DojoAppBar
+import tech.dojo.pay.uisdk.components.DojoFullGroundButton
+import tech.dojo.pay.uisdk.components.TitleGravity
+
+@Composable
+fun ManagePaymentMethods(
+    onCloseClicked:  () -> Unit,
+    onBackClicked: () -> Unit,
+    onNewCardButtonClicked: () -> Unit
+) {
+    DojoAppBar(
+        title = "Change payment method",
+        titleGravity = TitleGravity.LEFT,
+        navigationIcon = AppBarIcon.back { onBackClicked() },
+        actionIcon = AppBarIcon.close {
+            onCloseClicked()
+        }
+    )
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        val (doneBtn) = createRefs()
+        DojoFullGroundButton(
+            modifier = Modifier.constrainAs(doneBtn) {
+                start.linkTo(parent.start, 8.dp)
+                end.linkTo(parent.end, 8.dp)
+                bottom.linkTo(parent.bottom, 32.dp)
+                width = Dimension.fillToConstraints
+            },
+            text = "pay using new card"
+        ) { onNewCardButtonClicked() }
+    }
+}
