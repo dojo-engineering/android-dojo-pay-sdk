f08feca2d84b3ebc0b7d51dbc445f394c2b4fc7d
Saamyyy <saamyyps4@gmail.com>
2022-08-23 17:40:59 +0200

    add DojoBrandFooterWithTermsAndPrivacy

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/components/DojoBrandFooter.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/components/DojoBrandFooter.kt
@@ -1,7 +1,9 @@
 package tech.dojo.pay.uisdk.presentation.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Divider
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +20,9 @@ import tech.dojo.pay.uisdk.presentation.components.theme.bold
 @Composable
 internal fun DojoBrandFooter(
     modifier: Modifier = Modifier,
+    withTermsAndPrivacy: Boolean = false,
+    onTermsClicked: () -> Unit = {},
+    onPrivacyClicked: () -> Unit = {}
 ) {
     Column(modifier = modifier) {
         Row(
@@ -38,12 +43,45 @@ internal fun DojoBrandFooter(
             )
             Text(
                 text = stringResource(id = R.string.dojo_brand_footer_text_dojo),
-                modifier = Modifier.padding(4.dp),
+                modifier = Modifier.padding(start = 4.dp, top = 4.dp, bottom = 4.dp, end = 12.dp),
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
                 style = DojoTheme.typography.body1.bold,
                 color = LocalContentColor.current.copy(alpha = ContentAlpha.high)
             )
+            if (withTermsAndPrivacy) {
+                Divider(
+                    color = LocalContentColor.current.copy(alpha = ContentAlpha.high),
+                    modifier = Modifier
+                        .height(20.dp)
+                        .width(1.dp)
+                )
+                Text(
+                    text = "terms",
+                    modifier = Modifier
+                        .padding(
+                            start = 12.dp,
+                            top = 4.dp,
+                            bottom = 4.dp,
+                            end = 12.dp
+                        )
+                        .clickable { onTermsClicked.invoke() },
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = DojoTheme.typography.subtitle1,
+                    color = LocalContentColor.current.copy(alpha = ContentAlpha.high)
+                )
+                Text(
+                    text = "privacy",
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clickable { onPrivacyClicked.invoke() },
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = DojoTheme.typography.subtitle1,
+                    color = LocalContentColor.current.copy(alpha = ContentAlpha.high)
+                )
+            }
         }
     }
 }
@@ -52,4 +90,10 @@ internal fun DojoBrandFooter(
 @Composable
 internal fun PreviewDojoBrandFooter() = DojoPreview {
     DojoBrandFooter()
+}
+
+@Preview("DojoBrandFooter With Terms and privacy", group = "Footer")
+@Composable
+internal fun PreviewDojoBrandFooterWithTermsAndPrivacy() = DojoPreview {
+    DojoBrandFooter(withTermsAndPrivacy = true)
 }
