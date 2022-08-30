41accc3f3283c6a063fdeeea3e396aef12d30f88
Saamyyy <71371440+Saamyyy@users.noreply.github.com>
2022-08-30 16:15:43 +0200

    Feature/rmcx 1356 all strings in a translations file (#40)
    * add new strings and make all the flow read from strings file
    
    * update strings after rebase
    
    Co-authored-by: Saamyyy <saamyyps4@gmail.com>

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/mangepaymentmethods/ManagePaymentMethodsScreen.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/mangepaymentmethods/ManagePaymentMethodsScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import tech.dojo.pay.uisdk.R
 import tech.dojo.pay.uisdk.presentation.components.AppBarIcon
 import tech.dojo.pay.uisdk.presentation.components.DojoAppBar
 import tech.dojo.pay.uisdk.presentation.components.DojoBrandFooter
@@ -26,7 +28,7 @@ fun ManagePaymentMethods(
         color = Color.White
     ) {
         DojoAppBar(
-            title = "Change payment method",
+            title = stringResource(id = R.string.dojo_ui_sdk_manage_payment_methods_title),
             titleGravity = TitleGravity.LEFT,
             navigationIcon = AppBarIcon.back { onBackClicked() },
             actionIcon = AppBarIcon.close {
@@ -46,7 +48,7 @@ fun ManagePaymentMethods(
                     bottom.linkTo(footer.bottom, 46.dp)
                     width = Dimension.fillToConstraints
                 },
-                text = "pay using new card"
+                text =  stringResource(id = R.string.dojo_ui_sdk_card_details_checkout_title)
             ) { onNewCardButtonClicked() }
 
             DojoBrandFooter(
