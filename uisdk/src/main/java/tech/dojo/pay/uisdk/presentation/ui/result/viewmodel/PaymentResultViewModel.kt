fc38a3b03824473ce0401a69a52b329702f8bbbc
Saamyyy <saamyyps4@gmail.com>
2022-09-13 15:34:15 +0200

    add unit tests for the result screen

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/result/viewmodel/PaymentResultViewModel.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/result/viewmodel/PaymentResultViewModel.kt
@@ -4,8 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import tech.dojo.pay.sdk.DojoPaymentResult
 import tech.dojo.pay.uisdk.R
