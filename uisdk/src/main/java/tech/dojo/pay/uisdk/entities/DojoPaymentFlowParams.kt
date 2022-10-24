866ba5119760ce3d0e31f615fcf71aae0139b54e
Saamyyy <saamyyps4@gmail.com>
2022-10-24 13:29:29 +0200

    add clientSecret as optional parameter to DojoPaymentFlowParams

--- uisdk/src/main/java/tech/dojo/pay/uisdk/entities/DojoPaymentFlowParams.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/entities/DojoPaymentFlowParams.kt
@@ -5,5 +5,6 @@ import java.io.Serializable
 
 data class DojoPaymentFlowParams(
     val paymentId: String,
+    val clientSecret: String? = null,
     val GPayConfig: DojoGPayConfig? = null
 ) : Serializable
