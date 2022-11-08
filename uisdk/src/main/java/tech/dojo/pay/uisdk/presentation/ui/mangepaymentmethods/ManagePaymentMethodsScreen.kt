3dd9aa3cf3c7c06561110d4864a5731b19542a3b
Saamyyy <saamyyps4@gmail.com>
2022-11-08 16:10:15 +0200

    remove ConstraintLayout and use Scaffold

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/mangepaymentmethods/ManagePaymentMethodsScreen.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/mangepaymentmethods/ManagePaymentMethodsScreen.kt
@@ -1,21 +1,22 @@
 package tech.dojo.pay.uisdk.presentation.ui.mangepaymentmethods
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import tech.dojo.pay.uisdk.R
 import tech.dojo.pay.uisdk.presentation.components.*
 import tech.dojo.pay.uisdk.presentation.components.AppBarIcon
 import tech.dojo.pay.uisdk.presentation.components.DojoAppBar
 import tech.dojo.pay.uisdk.presentation.components.DojoBrandFooter
-import tech.dojo.pay.uisdk.presentation.components.DojoFullGroundButton
 import tech.dojo.pay.uisdk.presentation.components.DojoOutlinedButton
 import tech.dojo.pay.uisdk.presentation.components.TitleGravity
 import tech.dojo.pay.uisdk.presentation.components.theme.DojoTheme
@@ -33,6 +34,8 @@ internal fun ManagePaymentMethods(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
+        val scrollState = rememberScrollState()
+
         val state = viewModel.state.observeAsState().value ?: return@Surface
         if (state.showDialog) {
             SimpleAlertDialog(
@@ -44,72 +47,64 @@ internal fun ManagePaymentMethods(
                 onDismissButtonClicked = { viewModel.onCancelDialogClicked() }
             )
         }
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            val (appBar, walletItem, selectPaymentMethod, payWithNewCard, footer) = createRefs()
 
-            DojoAppBar(
-                modifier = Modifier.constrainAs(appBar) {
-                    start.linkTo(parent.start, 0.dp)
-                    end.linkTo(parent.end, 0.dp)
-                    top.linkTo(parent.top)
-                    width = Dimension.fillToConstraints
-                },
-                title = stringResource(id = R.string.dojo_ui_sdk_manage_payment_methods_title),
-                titleGravity = TitleGravity.LEFT,
-                navigationIcon = AppBarIcon.back(DojoTheme.colors.headerButtonTintColor) { onBackClicked() },
-                actionIcon = if (state.appBarIconType == AppBarIconType.CLOSE) {
-                    AppBarIcon.close(DojoTheme.colors.headerButtonTintColor) { onCloseClicked() }
-                } else {
-                    AppBarIcon.delete(DojoTheme.colors.headerButtonTintColor) { viewModel.onDeleteClicked() }
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            backgroundColor = Color.White,
+            topBar = {
+                DojoAppBar(
+                    title = stringResource(id = R.string.dojo_ui_sdk_manage_payment_methods_title),
+                    titleGravity = TitleGravity.LEFT,
+                    navigationIcon = AppBarIcon.back(DojoTheme.colors.headerButtonTintColor) { onBackClicked() },
+                    actionIcon = if (state.appBarIconType == AppBarIconType.CLOSE) {
+                        AppBarIcon.close(DojoTheme.colors.headerButtonTintColor) { onCloseClicked() }
+                    } else {
+                        AppBarIcon.delete(DojoTheme.colors.headerButtonTintColor) { viewModel.onDeleteClicked() }
+                    }
+                )
+            },
+            content = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                ) {
+                    Column(
+                        Modifier
+                            .padding(start = 8.dp, end = 8.dp, top = 16.dp, bottom = 200.dp),
+                        verticalArrangement = Arrangement.spacedBy(32.dp)
+                    ) {
+                        PaymentMethodsList(
+                            paymentMethodItems = state.paymentMethodItems.items,
+                            currentSelectedMethod = state.currentSelectedMethod,
+                            isInEditMode = state.isInEditMode,
+                            onItemChecked = { viewModel.onPaymentMethodChanged(it) },
+                            onItemLongClicked = { viewModel.onPaymentMethodLongCLick(it) }
+                        )
+                    }
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(0.dp),
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .background(Color.White),
+                    ) {
+                        SingleButtonView(
+                            text = stringResource(id = R.string.dojo_ui_sdk_pay_with_this_method),
+                            enabled = state.isUsePaymentMethodButtonEnabled
+                        ) { onBackClicked() }
+                        DojoOutlinedButton(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 4.dp),
+                            text = stringResource(id = R.string.dojo_ui_sdk_card_details_checkout_title)
+                        ) { onNewCardButtonClicked() }
+                        DojoBrandFooter(
+                            modifier = Modifier.padding(24.dp, 0.dp, 16.dp, 16.dp),
+                            withTermsAndPrivacy = false
+                        )
+                    }
                 }
-            )
-
-            PaymentMethodsList(
-                modifier = Modifier.constrainAs(walletItem) {
-                    start.linkTo(parent.start, 0.dp)
-                    end.linkTo(parent.end, 0.dp)
-                    top.linkTo(appBar.bottom)
-                    width = Dimension.fillToConstraints
-                },
-                paymentMethodItems = state.paymentMethodItems.items,
-                currentSelectedMethod= state.currentSelectedMethod,
-                isInEditMode = state.isInEditMode,
-                onItemChecked = { viewModel.onPaymentMethodChanged(it) },
-                onItemLongClicked = { viewModel.onPaymentMethodLongCLick(it) }
-            )
-
-            DojoOutlinedButton(
-                modifier = Modifier.constrainAs(selectPaymentMethod) {
-                    start.linkTo(parent.start, 16.dp)
-                    end.linkTo(parent.end, 16.dp)
-                    bottom.linkTo(payWithNewCard.bottom, 56.dp)
-                    width = Dimension.fillToConstraints
-                },
-                text = stringResource(id = R.string.dojo_ui_sdk_card_details_checkout_title)
-            ) { onNewCardButtonClicked() }
-            DojoFullGroundButton(
-                modifier = Modifier.constrainAs(payWithNewCard) {
-                    start.linkTo(parent.start, 16.dp)
-                    end.linkTo(parent.end, 16.dp)
-                    bottom.linkTo(footer.bottom, 46.dp)
-                    width = Dimension.fillToConstraints
-                },
-                text = stringResource(id = R.string.dojo_ui_sdk_pay_with_this_method),
-                enabled = state.isUsePaymentMethodButtonEnabled
-            ) { onBackClicked() }
-
-            DojoBrandFooter(
-                modifier = Modifier.constrainAs(footer) {
-                    start.linkTo(parent.start, 8.dp)
-                    end.linkTo(parent.end, 8.dp)
-                    bottom.linkTo(parent.bottom, 24.dp)
-                    width = Dimension.fillToConstraints
-                },
-                withTermsAndPrivacy = true
-            )
-        }
+            }
+        )
     }
 }
