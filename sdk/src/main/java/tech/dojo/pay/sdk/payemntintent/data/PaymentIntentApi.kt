cbfde705050778324770d0b87b7b9c49f2b29516
Saamyyy <saamyyps4@gmail.com>
2022-11-17 18:05:04 +0200

    update payment intent to the production  URLs

--- sdk/src/main/java/tech/dojo/pay/sdk/payemntintent/data/PaymentIntentApi.kt
+++ sdk/src/main/java/tech/dojo/pay/sdk/payemntintent/data/PaymentIntentApi.kt
@@ -3,17 +3,23 @@ package tech.dojo.pay.sdk.payemntintent.data
 import com.google.gson.JsonObject
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface PaymentIntentApi {
-    @GET("payment/{paymentId}")
+
+    @GET("payment-intents/public/{paymentId}")
     suspend fun fetchPaymentIntent(
-        @Path("paymentId") paymentId: String
+        @Path("paymentId") paymentId: String,
+        @Header("Version") version: String = API_VERSION
     ): Response<JsonObject>
 
-    @POST("payment/{paymentId}/refresh-client-session-secret")
+    @POST("payment-intents/public/{paymentId}/refresh-client-session-secret")
     suspend fun refreshPaymentIntent(
-        @Path("paymentId") paymentId: String
+        @Path("paymentId") paymentId: String,
+        @Header("Version") version: String = API_VERSION
     ): Response<JsonObject>
 }
+
+private const val API_VERSION = "2022-04-07"
