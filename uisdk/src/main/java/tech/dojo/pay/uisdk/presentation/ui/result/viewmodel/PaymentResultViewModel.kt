b80ef7178b049f1686c0bad90240e297728b839a
Saamyyy <saamyyps4@gmail.com>
2022-11-21 18:41:17 +0200

    add default value for the currentPaymentId in the view model for result

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/result/viewmodel/PaymentResultViewModel.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/result/viewmodel/PaymentResultViewModel.kt
@@ -19,7 +19,7 @@ internal class PaymentResultViewModel(
     private val refreshPaymentIntent: RefreshPaymentIntentUseCase
 ) : ViewModel() {
     private var currentState: PaymentResultState
-    lateinit var currentPaymentId: String
+    private var currentPaymentId: String = ""
     private val mutableState = MutableLiveData<PaymentResultState>()
     val state: LiveData<PaymentResultState>
         get() = mutableState
@@ -45,7 +45,11 @@ internal class PaymentResultViewModel(
     }
 
     private fun handlePaymentIntent(paymentIntentResult: PaymentIntentResult) {
-        if (paymentIntentResult is PaymentIntentResult.Success) { handlePaymentIntentSuccess(paymentIntentResult) } else if (paymentIntentResult is PaymentIntentResult.RefreshFailure) { handlePaymentIntentRefreshFailure() }
+        if (paymentIntentResult is PaymentIntentResult.Success) {
+            handlePaymentIntentSuccess(paymentIntentResult)
+        } else if (paymentIntentResult is PaymentIntentResult.RefreshFailure) {
+            handlePaymentIntentRefreshFailure()
+        }
     }
 
     private fun handlePaymentIntentSuccess(paymentIntentResult: PaymentIntentResult.Success) {
@@ -57,7 +61,7 @@ internal class PaymentResultViewModel(
                 status = R.string.dojo_ui_sdk_payment_result_title_success,
                 orderInfo = paymentIntentResult.result.id,
                 description = Currency.getInstance(paymentIntentResult.result.amount.currencyCode).symbol +
-                    paymentIntentResult.result.amount.valueString
+                        paymentIntentResult.result.amount.valueString
             )
         } else {
             PaymentResultState.FailedResult(
@@ -78,6 +82,7 @@ internal class PaymentResultViewModel(
         currentState = state
         postStateToUi(currentState)
     }
+
     private fun handlePaymentIntentRefreshFailure() {
         val state = PaymentResultState.FailedResult(
             appBarTitleId = R.string.dojo_ui_sdk_payment_result_title_fail,
