2a04c318e6b2c4f247c8a38e46d2b0f76603834d
Saamyyy <mahmoud.samy@paymentsense.com>
2023-02-16 11:29:26 +0000

    remove unneeded @ColorRes

--- uisdk/src/main/java/tech/dojo/pay/uisdk/entities/DojoThemeSettings.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/entities/DojoThemeSettings.kt
@@ -11,88 +11,48 @@ data class DojoThemeSettings(
 ) : Serializable
 
 data class LightColorPalette(
-    @ColorRes
     val primaryLabelTextColor: String = "#DD000000",
-    @ColorRes
     val secondaryLabelTextColor: String = "#99000000",
-    @ColorRes
     val headerTintColor: String = "#DD000000",
-    @ColorRes
     val headerButtonTintColor: String = "#99000000",
-    @ColorRes
     val primarySurfaceBackgroundColor: String = "#FFFFFFFF",
-    @ColorRes
     val primaryCTAButtonActiveBackgroundColor: String = "#FF000000",
-    @ColorRes
     val primaryCTAButtonActiveTextColor: String = "#FFFFFFFF",
-    @ColorRes
     val primaryCTAButtonDisabledBackgroundColor: String = "#FFE5E5E5",
-    @ColorRes
     val primaryCTAButtonDisableTextColor: String = "#60000000",
-    @ColorRes
     val secondaryCTAButtonActiveBorderColor: String = "#FF262626",
-    @ColorRes
     val secondaryCTAButtonActiveTextColor: String = "#FF262626",
-    @ColorRes
     val separatorColor: String = "#33000000",
-    @ColorRes
     val errorTextColor: String = "#FFB00020",
-    @ColorRes
     val loadingIndicatorColor: String = "#FF262626",
-    @ColorRes
     val inputFieldPlaceholderColor: String = "#FF000000",
-    @ColorRes
     val inputFieldBackgroundColor: String = "#FFFFFFFF",
-    @ColorRes
     val inputFieldDefaultBorderColor: String = "#26000000",
-    @ColorRes
     val inputFieldSelectedBorderColor: String = "#FF00857D",
-    @ColorRes
     val inputElementActiveTintColor: String = "#FF00857D",
-    @ColorRes
     val inputElementDefaultTintColor: String = "#26000000"
 ) : Serializable
 
 data class DarkColorPalette(
-    @ColorRes
     val primaryLabelTextColor: String = "#FFFFFFFF",
-    @ColorRes
     val secondaryLabelTextColor: String = "#FFFFFFFF",
-    @ColorRes
     val headerTintColor: String = "#FFFFFFFF",
-    @ColorRes
     val headerButtonTintColor: String = "#FFFFFFFF",
-    @ColorRes
     val primarySurfaceBackgroundColor: String = "#FF1B1B1B",
-    @ColorRes
     val primaryCTAButtonActiveBackgroundColor: String = "#FFFFFFFF",
-    @ColorRes
     val primaryCTAButtonActiveTextColor: String = "#DD000000",
-    @ColorRes
     val primaryCTAButtonDisabledBackgroundColor: String = "#FFC3C3C3",
-    @ColorRes
     val primaryCTAButtonDisableTextColor: String = "#60000000",
-    @ColorRes
     val secondaryCTAButtonActiveBorderColor: String = "#FFFFFFFF",
-    @ColorRes
     val secondaryCTAButtonActiveTextColor: String = "#FFFFFFFF",
-    @ColorRes
     val separatorColor: String = "#33000000",
-    @ColorRes
     val errorTextColor: String = "#FFED5645",
-    @ColorRes
     val loadingIndicatorColor: String = "#FFFFFFFF",
-    @ColorRes
     val inputFieldPlaceholderColor: String = "#FFFFFFFF",
-    @ColorRes
     val inputFieldBackgroundColor: String = "#FF313131",
-    @ColorRes
     val inputFieldDefaultBorderColor: String = "#26FFFFFF",
-    @ColorRes
     val inputFieldSelectedBorderColor: String = "#FFFFFFFF",
-    @ColorRes
     val inputElementActiveTintColor: String = "#FFFFFFFF",
-    @ColorRes
     val inputElementDefaultTintColor: String = "#FFFFFFFF"
 ) : Serializable
 
