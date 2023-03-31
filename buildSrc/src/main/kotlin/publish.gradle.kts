b8d83dfb8feede0a801bb202ca9fca9897946aff
Saamyyy <mahmoud.samy@paymentsense.com>
2023-03-31 13:02:18 +0100

    revert  the configs and rollback to local cardinal

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -52,21 +52,21 @@ tasks.dokkaJavadoc.configure {
 
 
 /**Create credentials.properties in root project folder file with gpr.user=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN**/
-val credentialProperties = Properties()
-credentialProperties.load(FileInputStream(rootProject.file("credentials.properties")))
+//val credentialProperties = Properties()
+//credentialProperties.load(FileInputStream(rootProject.file("credentials.properties")))
 
 afterEvaluate {
 
     publishing {
         repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/Dojo-Engineering/android-dojo-pay-sdk")
-                credentials {
-                    username = credentialProperties["gpr.user"] as String
-                    password = credentialProperties["gpr.key"] as String
-                }
-            }
+//            maven {
+//                name = "GitHubPackages"
+//                url = uri("https://maven.pkg.github.com/Dojo-Engineering/android-dojo-pay-sdk")
+//                credentials {
+//                    username = credentialProperties["gpr.user"] as String
+//                    password = credentialProperties["gpr.key"] as String
+//                }
+//            }
         }
 
         publications {
