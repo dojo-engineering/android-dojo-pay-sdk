9f486b61c8a6347ce841cd06f10ccba7bd9391ed
Saamyyy <mahmoud.samy@paymentsense.com>
2023-04-14 14:04:12 +0100

    remove  CardApiConfigurations

--- sdk/src/main/java/tech/dojo/pay/sdk/card/data/remote/baseurl/BaseUrlApiBuilder.kt
+++ sdk/src/main/java/tech/dojo/pay/sdk/card/data/remote/baseurl/BaseUrlApiBuilder.kt
@@ -5,7 +5,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import tech.dojo.pay.sdk.DojoSdk
-import tech.dojo.pay.sdk.card.entities.CardApiEnvironment
 import java.util.concurrent.TimeUnit
 
 internal object BaseUrlApiBuilder {
@@ -20,10 +19,10 @@ internal object BaseUrlApiBuilder {
             .client(createHttpClient())
             .build()
 
-    private fun getApiBaseUrl() = when (DojoSdk.cardApiConfigurations.cardApiEnvironment) {
-        CardApiEnvironment.PROD -> BASE_URL_GOOGLE_PROD_SAND_BOX
-        CardApiEnvironment.SAND_BOX -> BASE_URL_GOOGLE_PROD_SAND_BOX
-        CardApiEnvironment.STAGING -> BASE_URL_GOOGLE_SAND_BOX_STAGING
+    private fun getApiBaseUrl() = if (DojoSdk.isCardSandBox) {
+        BASE_URL_GOOGLE_SAND_BOX
+    } else {
+        BASE_URL_GOOGLE_PROD
     }
 
     private fun createHttpClient(): OkHttpClient =
@@ -33,7 +32,7 @@ internal object BaseUrlApiBuilder {
             .build()
 }
 
-private const val BASE_URL_GOOGLE_SAND_BOX_STAGING =
+private const val BASE_URL_GOOGLE_SAND_BOX =
     "https://storage.googleapis.com/remote-ag-nonprod-stg-manifest/"
-private const val BASE_URL_GOOGLE_PROD_SAND_BOX =
+private const val BASE_URL_GOOGLE_PROD =
     "https://storage.googleapis.com/remote-ag-prod-manifest/"
