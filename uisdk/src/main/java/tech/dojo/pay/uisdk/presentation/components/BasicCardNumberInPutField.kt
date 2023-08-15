229c575ba88a32cc2dd639b4f1c554e3861325f6
Saamyyy <mahmoud.samy@paymentsense.com>
2023-08-15 16:30:12 +0100

    fix UI Issues

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/components/BasicCardNumberInPutField.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/components/BasicCardNumberInPutField.kt
@@ -117,7 +117,7 @@ internal fun BasicCardNumberInputField(
         unfocusedBorderColor = DojoTheme.colors.inputFieldDefaultBorderColor,
         backgroundColor = DojoTheme.colors.inputFieldBackgroundColor,
         focusedBorderColor = DojoTheme.colors.inputFieldSelectedBorderColor,
-        placeholderColor = DojoTheme.colors.inputFieldPlaceholderColor,
+        placeholderColor = DojoTheme.colors.inputFieldPlaceholderColor.copy(alpha = 0.5f),
         errorBorderColor = DojoTheme.colors.errorTextColor,
         errorCursorColor = DojoTheme.colors.errorTextColor,
         errorLabelColor = DojoTheme.colors.errorTextColor,
