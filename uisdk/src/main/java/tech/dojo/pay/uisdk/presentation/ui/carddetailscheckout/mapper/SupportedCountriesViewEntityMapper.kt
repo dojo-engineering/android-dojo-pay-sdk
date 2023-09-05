4278c0eda640839fbbc65326dfc576034d202ce0
Saamyyy <mahmoud.samy@paymentsense.com>
2023-09-05 13:06:37 +0100

    add support for prsSet values on presentation

--- uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/carddetailscheckout/mapper/SupportedCountriesViewEntityMapper.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/presentation/ui/carddetailscheckout/mapper/SupportedCountriesViewEntityMapper.kt
@@ -4,11 +4,26 @@ import tech.dojo.pay.uisdk.domain.entities.SupportedCountriesDomainEntity
 import tech.dojo.pay.uisdk.presentation.ui.carddetailscheckout.entity.SupportedCountriesViewEntity
 
 internal class SupportedCountriesViewEntityMapper {
+
+    fun mapToSupportedCountriesViewEntityWithPreSelectedCountry(
+        supportedCountriesDomainEntityList: List<SupportedCountriesDomainEntity>,
+        preSelectedCountryCode: String?,
+    ): List<SupportedCountriesViewEntity> {
+        val preSelectedCountryIndex =
+            supportedCountriesDomainEntityList.indexOfFirst { it.countryCode == preSelectedCountryCode }
+        val mutableSupportedCountriesDomainEntityList = supportedCountriesDomainEntityList.toMutableList()
+        if (preSelectedCountryIndex != -1) {
+            val preSelectedCountry = mutableSupportedCountriesDomainEntityList.removeAt(preSelectedCountryIndex)
+            mutableSupportedCountriesDomainEntityList.add(0, preSelectedCountry)
+        }
+        return mutableSupportedCountriesDomainEntityList.map { apply(it) }
+    }
+
     fun apply(domainEntity: SupportedCountriesDomainEntity): SupportedCountriesViewEntity {
         return SupportedCountriesViewEntity(
             countryName = domainEntity.countryName,
             countryCode = domainEntity.countryCode,
-            isPostalCodeEnabled = domainEntity.isPostalCodeEnabled
+            isPostalCodeEnabled = domainEntity.isPostalCodeEnabled,
         )
     }
 }
