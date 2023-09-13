a4928f66c422ec8b65a27ac67e3120e86b9797b9
Saamyyy <mahmoud.samy@paymentsense.com>
2023-09-13 16:22:50 +0300

    make VT on Stagging

--- sample/src/main/java/tech/dojo/pay/sdksample/UiSdkSampleActivity.kt
+++ sample/src/main/java/tech/dojo/pay/sdksample/UiSdkSampleActivity.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import tech.dojo.pay.sdk.DojoPaymentResult
 import tech.dojo.pay.sdk.card.entities.DojoGPayConfig
+import tech.dojo.pay.sdk.card.entities.DojoSDKDebugConfig
+import tech.dojo.pay.sdk.card.entities.DojoSDKURLConfig
 import tech.dojo.pay.sdksample.customer.CustomerGenerator
 import tech.dojo.pay.sdksample.databinding.ActivityUiSdkSampleBinding
 import tech.dojo.pay.sdksample.token.PaymentIDGenerator
@@ -38,6 +40,7 @@ class UiSdkSampleActivity : AppCompatActivity() {
         }
         uiSdkSampleBinding.startPaymentFlowWithVT.setOnClickListener {
             DojoSDKDropInUI.dojoThemeSettings = DojoThemeSettings(forceLightMode = true)
+            DojoSDKDropInUI.dojoSDKDebugConfig = DojoSDKDebugConfig(urlConfig = DojoSDKURLConfig(remote = "https://staging-api.dojo.dev/integration-test/"))
             dojoPayUI.startPaymentFlow(
                 DojoPaymentFlowParams(
                     uiSdkSampleBinding.token.text.toString(),
