8d0ea50a8f36857f3f265edb78e6c50fd3bd2da7
Saamyyy <mahmoud.samy@paymentsense.com>
2023-10-25 14:32:40 +0100

    add urgent fix for pro-Gaurd in the UI SDK

--- uisdk/src/main/java/tech/dojo/pay/uisdk/data/entities/PaymentMethodsRaw.kt
+++ uisdk/src/main/java/tech/dojo/pay/uisdk/data/entities/PaymentMethodsRaw.kt
@@ -1,21 +1,24 @@
 package tech.dojo.pay.uisdk.data.entities
 
+import androidx.annotation.Keep
 import tech.dojo.pay.sdk.card.entities.CardsSchemes
-
+@Keep
 internal data class PaymentMethodsRaw(
     val customerId: String,
     val merchantId: String,
     val savedPaymentMethods: List<SavedPaymentMethods>,
-    val supportedPaymentMethods: SupportedPaymentMethods
+    val supportedPaymentMethods: SupportedPaymentMethods,
 )
 
+@Keep
 internal data class SavedPaymentMethods(
     val id: String,
-    val cardDetails: CardDetails
+    val cardDetails: CardDetails,
 )
 
+@Keep
 internal data class CardDetails(
     val pan: String,
     val expiryDate: String,
-    val scheme: CardsSchemes
+    val scheme: CardsSchemes,
 )
