6ece09eeeb42bd5710ec4791a59170013e927d5f
Den <deniss.kaibagarovs@paymentsense.com>
2024-03-20 20:47:31 +0000

    additional legal text on the checkout page

--- sample/src/main/java/tech/dojo/pay/sdksample/UiSdkSampleActivity.kt
+++ sample/src/main/java/tech/dojo/pay/sdksample/UiSdkSampleActivity.kt
@@ -32,6 +32,10 @@ class UiSdkSampleActivity : AppCompatActivity() {
 
         uiSdkSampleBinding.startPaymentFlow.setOnClickListener {
             DojoSDKDropInUI.dojoThemeSettings = DojoThemeSettings(forceLightMode = false)
+            if (uiSdkSampleBinding.checkboxAdditonalLegalText.isChecked) {
+                DojoSDKDropInUI.dojoThemeSettings?.additionalLegalText =
+                    "Dojo is a trading name of Paymentsense Limited. Copyright ©2024 Paymentsense Limited. All rights reserved. Paymentsense Limited is authorised and regulated by the Financial Conduct Authority (FCA FRN 738728) and under the Electronic Money Regulations 2011 (FCA FRN 900925) for the issuing of electronic money and provision of payment services. Our company number is 06730690 and our registered office address is The Brunel Building, 2 Canalside Walk, London W2 1DG"
+            }
             dojoPayUI.startPaymentFlow(
                 DojoPaymentFlowParams(uiSdkSampleBinding.token.text.toString()),
             )
