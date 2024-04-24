fcf5be648355f7fd6b93d46c9ae24ea845eb8696
Saamyyy <mahmoud.samy@paymentsense.com>
2024-04-24 23:12:37 +0100

    disable relase mode

--- sample/src/main/java/tech/dojo/pay/sdksample/token/PaymentIntentApi.kt
+++ sample/src/main/java/tech/dojo/pay/sdksample/token/PaymentIntentApi.kt
@@ -1,16 +1,10 @@
 package tech.dojo.pay.sdksample.token
 
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface PaymentIntentApi {
 
-    @Headers(
-        "Authorization:Basic sk_sandbox_b-pGLxFNm_kEr1aEWZcFp9HQu11wey7ucc48Y1e4-nZdGxhAJY3Bgx2Eb_C-itA16bSnojrZKesvnoZAoiRtPA",
-        "Version: 2022-10-18",
-        "Content-Type:application/json"
-    )
     @POST("payment-intents")
     suspend fun getToken(@Body params: PaymentIdBody): PaymentIdResponse
 }
