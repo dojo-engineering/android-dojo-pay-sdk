90bb7624cbc40601b1a162eff001147239c5ea37
francescoDandrea <francesco.dandrea@dojo.tech>
2024-09-26 11:38:51 +0100

    updating script to sdk release

--- scripts/tag_ui_sdk_release.sh
+++ scripts/tag_ui_sdk_release.sh
@@ -4,22 +4,52 @@
 BUILD_FILE="./uisdk/build.gradle.kts"
 
 # Extract the version from build.gradle.kts from line 1
-VERSION=$(head -n 1 "$BUILD_FILE")
-VERSION=$(echo "$VERSION" | sed 's/"//g')
-VERSION=$(echo "$VERSION" | sed 's/version = //g')
+currentVersion=$(head -n 1 "$BUILD_FILE")
+currentVersion=$(echo "$currentVersion" | sed 's/"//g')
+currentVersion=$(echo "$currentVersion" | sed 's/version = //g')
 
-# Check if the version was found
-if [ -z "$VERSION" ]; then
-    echo "Version not found in $BUILD_FILE"
+modifiedFiles=$(git ls-files -m)
+modifiedFiles=''
+if [ -n "$modifiedFiles" ]; then
+  echo "You have local changes to this files:"
+  echo "$modifiedFiles"
+else
+
+  echo "Insert the new version name (x.y.z), current version is $currentVersion\n"
+  read -r inputVersionName
+  # Check if the input is empty
+  if [[ -z "$inputVersionName" ]]; then
+    echo "Error: Version name cannot be empty."
     exit 1
+  fi
+
+  echo ""
+  echo "Updating local branch..."
+  git pull origin
+
+  currentBranch=$(git rev-parse --abbrev-ref HEAD)
+
+  echo "Current branch $currentBranch"
+  echo ""
+
+  sed -i.bak "s/version = \".*\"/version = \"$inputVersionName\"/" "$BUILD_FILE"
+  rm "$BUILD_FILE.bak"
+
+  echo ""
+  echo "Version will be updated from '$currentVersion' to '$inputVersionName', release tag will be created and pushed!"
+  echo "Continue? y/n"
+  read -r response
+  case "$response" in
+  [yY][eE][sS] | [yY])
+    echo "Commit in progress... $inputVersionName"
+    git commit $BUILD_FILE -m "bumped version $inputVersionName"
+    git push
+    git tag -a "tag/ui_v$inputVersionName" -m ''
+    git push origin "RC_$inputVersionName"
+    ;;
+  *)
+  git restore $BUILD_FILE
+    echo "Bye Bye"
+    ;;
+  esac
 fi
-
-TAG_NAME="ui-v$VERSION"
-
-# Create a git tag with the version
-git tag "$TAG_NAME"
-
-# Push the tag to the remote repository (optional, uncomment to enable)
-git push origin "$TAG_NAME"
-
-echo "Git tag '$TAG_NAME'created successfully."
